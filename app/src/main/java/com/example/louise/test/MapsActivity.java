6417cb4bc6f73325c8c3a442741369c4c4a4bd7f
林如意 <LOUISE@linruyis-MacBook-Pro.local>
2016-03-23 16:51:14 +0800

    Add MapsActivty about coin_icon to be the CoinActivity Prototype

--- app/src/main/java/com/example/louise/test/MapsActivity.java
+++ app/src/main/java/com/example/louise/test/MapsActivity.java
@@ -41,10 +41,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import org.json.JSONArray;
 import org.json.JSONException;
 import java.net.ProtocolException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -247,46 +249,93 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             try {
 
                 if (placelist.getJSONObject(i).getInt("mainid") == 0) {
-                    Double latitudea = placelist.getJSONObject(i).getDouble("latitude");
-                    Double longitude = placelist.getJSONObject(i).getDouble("longitude");
-                    final LatLng po = new LatLng(latitudea, longitude);
-                    options.position(po);
-                    options.title(placelist.getJSONObject(i).getString("name"));
-//                    修改水滴的圖示
-                    String stelejson = "";
-                    try {
-                        int id = placelist.getJSONObject(i).getInt("id");
-                        stelejson = Httpconnect.httpget("http://140.119.163.40:8080/Spring08/app/stele/" + id);
-                    } catch (ProtocolException e) {
-                        e.printStackTrace();
+
+//                    Double latitudea = placelist.getJSONObject(i).getDouble("latitude");
+//                    Double longitude = placelist.getJSONObject(i).getDouble("longitude");
+
+                    Double maxlat = 24.988155;
+                    Double minlat = 24.983055;
+                    Double maxlon = 121.577579;
+                    Double minlon = 121.573079;
+                    Double resultlat = 0.0;
+                    Double resultlon = 0.0;
+                    Random random = new Random();
+                    for(int ii =0; ii < 1; ii++) {
+                        resultlat = minlat + random.nextDouble() * (maxlat - minlat);
+                        resultlat = ((int) (resultlat * 1000000)) / 1000000.0;
                     }
-                    String camp = "";
-                    try {
-                        JSONArray stelelist = new JSONArray(stelejson);
-                        camp = stelelist.getJSONObject(0).getString("camp");
-                        if(camp.equals("玩家")) {
-                            switch (StoryActivity.party) {
-                                case "Sinae": {
-                                    BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.blueflag);
+                    for(int ii =0; ii < 1; ii++) {
+                        resultlon = minlon + random.nextDouble() * (maxlon - minlon);
+                        resultlon = ((int) (resultlon * 1000000)) / 1000000.0;
+                    }
+
+                    List<Double> latitude = new ArrayList<>();
+                    latitude.add(placelist.getJSONObject(i).getDouble("latitude"));
+                    latitude.add(resultlat);
+
+                    List<Double> longitude = new ArrayList<>();
+                    longitude.add(placelist.getJSONObject(i).getDouble("longitude"));
+                    longitude.add(resultlon);
+
+//                    Toast.makeText(getApplicationContext(), resultlat.toString() + ", " + resultlon.toString(), Toast.LENGTH_SHORT).show();
+                    int ss = latitude.size();
+                    for (int l = 0; l < ss; l++) {
+
+
+                        final LatLng po;
+                        if (latitude.get(l).equals(placelist.optJSONObject(i).getDouble("latitude")) && longitude.get(l).equals(placelist.getJSONObject(i).getDouble("longitude"))) {
+                            po = new LatLng(latitude.get(0), longitude.get(0));
+//                        Toast.makeText(getApplicationContext(), po.toString(), Toast.LENGTH_SHORT).show();
+                            options.position(po);
+                            options.title(placelist.getJSONObject(i).getString("name"));
+//                    修改水滴的圖示
+                            String stelejson = "";
+                            try {
+                                int id = placelist.getJSONObject(i).getInt("id");
+                                stelejson = Httpconnect.httpget("http://140.119.163.40:8080/Spring08/app/stele/" + id);
+                            } catch (ProtocolException e) {
+                                e.printStackTrace();
+                            }
+                            String camp = "";
+                            try {
+                                JSONArray stelelist = new JSONArray(stelejson);
+                                camp = stelelist.getJSONObject(0).getString("camp");
+                                if (camp.equals("玩家")) {
+                                    switch (StoryActivity.party) {
+                                        case "Sinae": {
+                                            BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.blueflag);
+                                            options.icon(icon);
+                                            break;
+                                        }
+                                        default: {
+                                            BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.redflag);
+                                            options.icon(icon);
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.blackflag);
                                     options.icon(icon);
-                                    break;
                                 }
-                                default: {
-                                    BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.redflag);
-                                    options.icon(icon);
-                                    break;
-                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
                         } else {
-                            BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.blackflag);
+                            po = new LatLng(latitude.get(1), longitude.get(1));
+//                            Toast.makeText(getApplicationContext(), po.toString(), Toast.LENGTH_SHORT).show();
+                            options.position(po);
+                            options.title("");
+                            BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.coin);
                             options.icon(icon);
                         }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    mMap.addMarker(options);
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(po, 17));
+//                    LatLng pocoin = new LatLng(24.9871552, 121.57357);
+//                    options.position(pocoin);
+//                    BitmapDescriptor coin_icon = BitmapDescriptorFactory.fromResource(R.drawable.coin);
+//                    options.icon(coin_icon);
 
+                        mMap.addMarker(options);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(po, 17));
+                    }
 
 //                    if((int)mMap.getCameraPosition().zoom < 15) {
 //                        AlertDialog.Builder ad = new AlertDialog.Builder(MapsActivity.this);
